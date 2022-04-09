a5cc32bfcb502b0c1396c275a8f465464dbfd3c9
gqin10 <swe1904862@xmu.edu.my>
2022-04-10 01:23:33 +0800

    add function to receive user input

--- Notebook.py
+++ Notebook.py
@@ -1,15 +1,56 @@
+from enum import Enum
+
+
+class NATURE(Enum):
+    NEAR = "near"
+    EQUAL = "equal"
+    MORE = "more"
+    LESS = "less"
+
+
+class Attribute:
+    def __init__(self, nature):
+        self.priority = 0
+        self.nature = nature
+
+    def __str__(self) -> str:
+        res = ""
+        for key, value in self.__dict__.items():
+            res += f"\t{key}: {value}\n"
+        return res
+
+
+class String_Attribute(Attribute):
+    def __init__(self, nature):
+        self.value = ""
+        super().__init__(nature)
+
+
+class Number_Attribute(Attribute):
+    def __init__(self, nature):
+        self.min_value = 0
+        self.max_value = 0
+        super().__init__(nature)
+
+
 class Notebook:
     def __init__(self):
-        self.brand = ""
+        self.brand = String_Attribute(NATURE.EQUAL)
         self.model = ""
         self.cpu_brand = ""
-        self.cpu = ""
-        self.ram = ""
-        self.storage = ""
-        self.screen_size = ""
-        self.gpu = ""
-        self.weight = ""
+        self.cpu = String_Attribute(NATURE.NEAR)
+        self.ram = Number_Attribute(NATURE.MORE)
+        self.storage = Number_Attribute(NATURE.MORE)
+        self.screen_size = Number_Attribute(NATURE.NEAR)
+        self.gpu = String_Attribute(NATURE.MORE)
+        self.weight = Number_Attribute(NATURE.EQUAL)
         self.height = ""
         self.width = ""
         self.depth = ""
-        self.price = ""
+        self.price = Number_Attribute(NATURE.LESS)
+
+    def __str__(self) -> str:
+        res = ""
+        for key, value in self.__dict__.items():
+            res += f"{key}:\n{value}\n"
+        return res
--- main.py
+++ main.py
@@ -1,24 +1,81 @@
 from Notebook import Notebook
 
+
+def get_requirements():
+    user_constraint = Notebook()
+
+    user_constraint.brand.priority = input("Enter brand priority: ")
+    if float(user_constraint.brand.priority) > 0:
+        user_constraint.brand.value = input("Enter brand option: ")
+
+    user_constraint.cpu.priority = input("Enter CPU priority: ")
+    if float(user_constraint.cpu.priority) > 0:
+        user_constraint.cpu.value = input("Enter CPU option: ")
+
+    user_constraint.gpu.priority = input("Enter GPU priority")
+    if float(user_constraint.gpu.priority) > 0:
+        user_constraint.gpu.value = input("Enter CPU option: ")
+
+    user_constraint.ram.priority = input("Enter RAM priority: ")
+    if float(user_constraint.ram.priority) > 0:
+        user_constraint.ram.min_value = input("Enter minimum RAM (GB): ")
+        user_constraint.ram.max_value = input("Enter maximum RAM (GB): ")
+
+    user_constraint.storage.priority = input("Enter storage priority: ")
+    if float(user_constraint.storage.priority) > 0:
+        user_constraint.storage.min_value = input("Enter minimum storage (GB): ")
+        user_constraint.storage.max_value = input("Enter maximum storage (GB): ")
+
+    user_constraint.screen_size.priority = input("Enter screen size priority: ")
+    if float(user_constraint.screen_size.priority) > 0:
+        user_constraint.screen_size.min_value = input("Enter minimum screen size (inches): ")
+        user_constraint.screen_size.max_value = input("Enter maximum screen size (inches): ")
+
+    user_constraint.weight.priority = input("Enter weight priority: ")
+    if float(user_constraint.weight.priority) > 0:
+        user_constraint.weight.min_value = input("Enter minimum weight (kg): ")
+        user_constraint.weight.max_value = input("Enter maximum weight(kg): ")
+
+    user_constraint.price.priority = input("Enter price priority: ")
+    if float(user_constraint.price.priority) > 0:
+        user_constraint.price.min_value = input("Enter minimum price (RM): ")
+        user_constraint.price.max_value = input("Enter maximum price (RM): ")
+
+    return user_constraint
+
+
 if __name__ == "__main__":
     print("Refer to notebook-spec-option.md for the available options")
 
-    user_constraint = Notebook()
-
-    user_constraint.brand = ""
-    user_constraint.cpu = ""
-    user_constraint.ram = ""
-    user_constraint.storage = ""
-    user_constraint.screen_size = ""
-    user_constraint.weight = ""
-    user_constraint.price = ""
+    user_constraint = get_requirements()
 
     """
-    user_constraint.brand = input("Select brand: ") 
-    user_constraint.cpu = input("Select cpu: ")
-    user_constraint.ram = input("Enter range of RAM: ") 
-    user_constraint.storage = input("Enter range of storage: ") 
-    user_constraint.screen_size = input("Enter range of screen size: ") 
-    user_constraint.weight = input("Enter range of weight: ") 
-    user_constraint.price = input("Enter range of price") 
+    user_constraint.brand.value = "HP"
+    user_constraint.brand.priority = 1 
+    
+    user_constraint.cpu.value = "i3"
+    user_constraint.cpu.priority = 1
+    
+    user_constraint.gpu.value = "rtx"
+    user_constraint.gpu.priority = 1
+    
+    user_constraint.ram.min_value = 8
+    user_constraint.ram.max_value = 16
+    user_constraint.ram.priority = 2 
+    
+    user_constraint.storage.min_value = 256
+    user_constraint.storage.max_value = 512
+    user_constraint.storage.priority = 3 
+    
+    user_constraint.screen_size.min_value = 13
+    user_constraint.screen_size.max_value = 15
+    user_constraint.screen_size.priority = 1 
+    
+    user_constraint.weight.min_value = 1.5
+    user_constraint.weight.max_value = 3
+    user_constraint.screen_size.priority = 1 
+    
+    user_constraint.price.min_value = 1000
+    user_constraint.price.max_value = 5000
+    user_constraint.screen_size.priority = 1 
     """
