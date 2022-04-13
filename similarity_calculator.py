be2fb99467a81384f25450cb296c8848e54e4662
gqin10 <swe1904862@xmu.edu.my>
2022-04-14 00:17:03 +0800

    add similarity_calculator

--- similarity_calculator.py
+++ similarity_calculator.py
@@ -1,9 +1,16 @@
+import pandas as pd
 from Notebook import String_Attribute, Number_Attribute
 
+def average(a, b):
+    return (a+b)/2
+
 def compute_similarity(constraint, item_list):
+    #assign 0 to each item's similarity
+    item_list["similarity"] = [0 for i in range((item_list.shape)[0])]
+
     for key, attr in constraint.__dict__.items():
         if isinstance(attr, str) or attr.priority <= 0:
-            pass
+            continue
 
         if isinstance(attr, String_Attribute) and attr.value != "":
             # process string constraint
@@ -11,4 +18,7 @@ def compute_similarity(constraint, item_list):
 
         elif isinstance(attr, Number_Attribute) and (attr.min_value > 0 or attr.max_value > 0):
             # process number constraint
-            pass
+            s = attr.priority * (average(attr.min_value, attr.max_value) * item_list[key].astype(float))
+            item_list["similarity"] = item_list["similarity"] + s
+
+    print(item_list)
