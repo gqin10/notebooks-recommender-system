38378b3e7e4fbc18e5b6e22628b197bba4ac552b
gqin10 <swe1904862@xmu.edu.my>
2022-04-20 17:53:32 +0800

    add failing test constraint

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from item_extrator import extract_notebooks
 from similarity_calculator import compute_similarity
 from constraint_relax import relax_constraints
 from user_interaction import get_user_requirements
-from test.test_constraint import get_test_constraint
+from test.test_constraint import *
 
 import pandas as pd
 
@@ -15,15 +15,19 @@ if __name__ == "__main__":
     print("Refer to notebook-spec-option.md for the available options")
 
     # user_constraint = get_user_requirements()
-    user_constraint = get_test_constraint()
-    matching_notebooks = extract_notebooks(user_constraint) # extract items that fulfil constraints
+    # user_constraint = get_test_constraint()
+    user_constraint = get_fail_constraint()
+
+    matching_notebooks = extract_notebooks(user_constraint)  # extract items that fulfil constraints
 
     # relax constraint if  no items can be found
     # matching_notebooks, dropped_constraint = relax_constraints(user_constraint)
 
     if (matching_notebooks.shape)[0] > 0:
-        user_constraint = use_cpu_average(user_constraint) if not is_using_cpu_average(user_constraint) else user_constraint
-        user_constraint = use_gpu_average(user_constraint) if not is_using_gpu_average(user_constraint) else user_constraint
+        user_constraint = use_cpu_average(user_constraint) if not is_using_cpu_average(
+            user_constraint) else user_constraint
+        user_constraint = use_gpu_average(user_constraint) if not is_using_gpu_average(
+            user_constraint) else user_constraint
         matching_notebooks = compute_similarity(user_constraint, matching_notebooks)
         matching_notebooks = matching_notebooks.sort_values(by=['similarity'], ascending=False)
 
--- test/test_constraint.py
+++ test/test_constraint.py
@@ -10,9 +10,9 @@ def get_test_constraint():
     user_constraint.cpu.priority = 5
     user_constraint.gpu.value = ["rtx"]
     user_constraint.gpu.priority = 10
-    user_constraint.ram.min_value = 8
+    user_constraint.ram.min_value = 16
     user_constraint.ram.max_value = 16
-    user_constraint.ram.priority = 1
+    user_constraint.ram.priority = 4
     user_constraint.storage.min_value = 0
     user_constraint.storage.max_value = 0
     user_constraint.storage.priority = 0
@@ -22,6 +22,33 @@ def get_test_constraint():
     user_constraint.weight.min_value = 0
     user_constraint.weight.max_value = 0
     user_constraint.weight.priority = 0
+    user_constraint.price.min_value = 0
+    user_constraint.price.max_value = 0
+    user_constraint.price.priority = 0
+
+    return set_cpu_gpu_average(user_constraint)
+
+def get_fail_constraint():
+    user_constraint = Constraint()
+
+    user_constraint.brand.value = []
+    user_constraint.brand.priority = 0
+    user_constraint.cpu.value = ["celeron"]
+    user_constraint.cpu.priority = 5
+    user_constraint.gpu.value = ["rtx"]
+    user_constraint.gpu.priority = 10
+    user_constraint.ram.min_value = 8
+    user_constraint.ram.max_value = 8
+    user_constraint.ram.priority = 4
+    user_constraint.storage.min_value = 0
+    user_constraint.storage.max_value = 0
+    user_constraint.storage.priority = 0
+    user_constraint.screen_size.min_value = 0
+    user_constraint.screen_size.max_value = 0
+    user_constraint.screen_size.priority = 0
+    user_constraint.weight.min_value = 1.5
+    user_constraint.weight.max_value = 2
+    user_constraint.weight.priority = 4
     user_constraint.price.min_value = 0
     user_constraint.price.max_value = 0
     user_constraint.price.priority = 0
