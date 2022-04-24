d630bdeaae2f580942a39074d02ed5a9ee74f1d7
gqin10 <swe1904862@xmu.edu.my>
2022-04-24 17:40:11 +0800

    remove extra lines in test_constraint.py

--- test/test_constraint.py
+++ test/test_constraint.py
@@ -11,18 +11,13 @@ def get_test_constraint():
     user_constraint.gpu.value = ["rtx"]
     user_constraint.gpu.priority = 10
     user_constraint.ram.value = 16
-    user_constraint.ram.value = 16
     user_constraint.ram.priority = 4
-    user_constraint.storage.value = 0
     user_constraint.storage.value = 0
     user_constraint.storage.priority = 0
     user_constraint.screen_size.value = 14
-    user_constraint.screen_size.value = 17.2
     user_constraint.screen_size.priority = 4
     user_constraint.weight.value = 0
-    user_constraint.weight.value = 0
     user_constraint.weight.priority = 0
-    user_constraint.price.value = 0
     user_constraint.price.value = 0
     user_constraint.price.priority = 0
 
