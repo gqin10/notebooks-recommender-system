dfdc3b9eae006262bc8af59fcca9aa91254e0143
gqin10 <swe1904862@xmu.edu.my>
2022-05-04 18:11:42 +0800

    update spec_list.py

--- spec_list.py
+++ spec_list.py
@@ -1,43 +1,8 @@
-spec_list = [
-    "brand", "cpu", "ram",
-    "storage", "screen_size", "gpu", "weight",
-    "price"
-]
-
-"""
-# Brand
-0. Acer
-1. Asus
-2. Dell
-3. HP
-4. Huawei
-5. Lenovo
-6. Microsoft
-7. MSI
-"""
-brand_list = ['acer', 'asus', 'dell', 'hp', 'huawei', 'lenovo', 'microsoft', 'msi']
-
-"""
-# CPU
-0. Intel Celeron
-1. Intel Pentium
-2. Intel Core i3
-3. Intel Core i5
-4. Intel Core i7 
-5. AMD Athlon
-6. AMD Ryzen 3
-7. AMD Ryzen 5
-8. AMD Ryzen 7
-9. AMD Ryzen 9
-"""
-cpu_list = ['celeron', 'pentium', 'i3', 'i5', 'i7', 'athlon', 'ryzen 3', 'ryzen 5', 'ryzen 7', 'ryzen 9']
-
-"""
-# GPU
-0. AMD Radeon
-1. Intel Iris
-2. Nvidia MX
-3. Nvidia GTX
-4. Nvidia RTX
-"""
-gpu_list = ['radeon', 'iris', 'mx', 'gtx', 'rtx']
+spec = {
+    'brand': ['acer', 'asus', 'dell', 'hp', 'huawei', 'lenovo', 'microsoft', 'msi'],
+    'cpu': ['celeron', 'athlon', 'pentium', 'i3', 'ryzen 3', 'i5', 'ryzen 5', 'i7', 'ryzen 7', 'ryzen 9'],
+    'gpu': ['radeon', 'iris', 'mx', 'gtx', 'rtx'],
+    'storage': [32, 64, 128, 216, 512, 1024, 2048],
+    'ram': [2, 4, 8, 16, 32, 64],
+    'os': ['os', '10', '11']
+}
