8ab086f3218409e7e0aec66b0c254b7e9de71769
gqin10 <swe1904862@xmu.edu.my>
2022-05-16 21:12:10 +0800

    edit spec_list.py

--- spec_list.py
+++ spec_list.py
@@ -2,7 +2,7 @@ spec = {
     'brand': ['acer', 'asus', 'dell', 'hp', 'huawei', 'lenovo', 'microsoft', 'msi'],
     'cpu': ['celeron', 'athlon', 'pentium', 'i3', 'ryzen 3', 'i5', 'ryzen 5', 'i7', 'ryzen 7', 'ryzen 9'],
     'gpu': ['radeon', 'iris', 'mx', 'gtx', 'rtx'],
-    'storage': [32, 64, 128, 216, 512, 1024, 2048],
+    'storage': [32, 64, 128, 256, 512, 1024, 2048],
     'ram': [2, 4, 8, 16, 32, 64],
     'os': ['os', '10', '11']
 }
